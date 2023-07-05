b700e2fe2d29bde825013c95a210f18fe652df4e
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-05 23:06:33 +0300

    Calculations Complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31, num2 = 2
+const multiply = (num1 * num2)
+
+let random = Math.ceil(Math.random());
+
+let num3 = 19, num4 = 5
+const mod = (num3 % num4)
+
+let obj = (1,4,6,12,13,20)
+const max = Math.max(obj)
